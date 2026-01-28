f9233dc9242db93b1f2b8ff9e13c6d27171cb0aa
rtaugourdeau-LPB <r.taugourdeau@lapremierebrique.fr>
2026-01-28 14:41:24 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -398,14 +398,21 @@ if page == "Prérequis":
 elif page == "Transposition PDP":
     st.header("📥 Transposition PDP (cloud)")
 
-    st.markdown(
-        """
-1. Importez l’export Typeform (Excel).  
-2. L’outil crée 1 fichier Excel **par porteur**, rangé par **mois**,  
-   et sépare **France** / **Localisation non adaptée**.  
-3. Vous pouvez télécharger un **ZIP** et l’onglet *Emailing* est mis à jour.
+st.markdown(
+    """
+### 1) Importez l’export Typeform (Excel)
+Données test : [Télécharger le fichier](https://lapremierebriquelpb-my.sharepoint.com/:x:/g/personal/r_taugourdeau_lapremierebrique_fr/IQDJJ1vtSZR5SJw8oTjp0LPVAVrFhT-pLWh9Zk1_Bk_Q69Y?e=L1aRZx)
+
+### 2) Génération des fichiers
+L’outil crée **un fichier Excel par porteur**, rangé par **mois**, et sépare :
+- **France**
+- **Localisation non adaptée**
+
+### 3) Téléchargement
+Vous pouvez télécharger un **ZIP**, et l’onglet *Emailing* est mis à jour.
 """
-    )
+)
+
 
     uploaded = st.file_uploader(
         "Déposez l’Excel (ex: Réponse formulaire PDP.xlsx)",
@@ -864,6 +871,7 @@ elif page == "Emailing":
                 f"🎯 Terminé : {ok_count} mail(s) envoyé(s), {err_count} erreur(s)."
 
             )
+
 
 
 
